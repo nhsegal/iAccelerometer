2588e19b11f0259607f409e76ef88aee90af4551
Noah Segal <n.h.segal@gmail.com>
2023-06-19 14:55:26 +0900

    button size

--- sketch.js
+++ sketch.js
@@ -36,7 +36,7 @@ function requestAccess() {
   button.remove();
   scaleUpButton = createButton("+");
   scaleUpButton.style("font-size", "32px");
-  scaleUpButton.position(12, 115);
+  scaleUpButton.position(12, 125);
   scaleUpButton.style("transform", "rotate(90deg)");
   scaleUpButton.style("text-align", "center");
   scaleUpButton.mousePressed(() => {
